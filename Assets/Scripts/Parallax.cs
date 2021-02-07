1bfa84adc2df133a8d46c867d9267975c05a692f
Emily Bender <bender.emily1@gmail.com>
2021-02-06 22:37:50 -0500

    Infinite scrolling, cave tile palette

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,41 +7,62 @@ public class Parallax : MonoBehaviour
     public Camera cam;
     public Transform player;
 
+    // Instance variables
     Vector2 startPosition;
     float startZPos;
-    private float length; 
 
-    /* => denotes Property variable
-    * Calculated everytime it is called 
-    * Read only
-    */
+    public SpriteRenderer loopSpriteRenderer;
+
+    float twoAspect => cam.aspect * 2;
+    float tileWidth => (twoAspect > 3 ? twoAspect : 3);
+    float viewWidth => loopSpriteRenderer.sprite.rect.width / loopSpriteRenderer.sprite.pixelsPerUnit;
     Vector2 travel => (Vector2)cam.transform.position - startPosition;
-
     float distanceFromSubject => transform.position.z - player.position.z;
-
-    // If farther to the camera than the player use farther clipping plane
+    // If farther from the camera than the player use farther clipping plane for parallax factor, else use near clipping plane
     float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
 
-    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane; 
-    // Start is called before the first frame update
-    void Start()
+    //User options 
+    public bool xAxis = true; // parallax on x?
+    public bool yAxis = true; // parallax on y?
+    public bool infiniteLoop = false; //do we loop?
+
+ 
+    void Awake()
     {
+        cam = Camera.main;
         startPosition = transform.position;
         startZPos = transform.position.z;
 
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        if(loopSpriteRenderer != null && infiniteLoop)
+        {
+            float spriteSizeX = loopSpriteRenderer.sprite.rect.width / loopSpriteRenderer.sprite.pixelsPerUnit;
+            float spriteSizeY = loopSpriteRenderer.sprite.rect.height / loopSpriteRenderer.sprite.pixelsPerUnit;
+
+            // Change draw mode to titled so sprite can be draw multiple times  
+            loopSpriteRenderer.drawMode = SpriteDrawMode.Tiled;
+            // Increase sprite renderer size to allow for tiles to be tiled
+            loopSpriteRenderer.size = new Vector2(spriteSizeX * tileWidth, spriteSizeY);
+            // Ensure scale of sprite remains the same 
+            transform.localScale = Vector3.one;
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x) * (1 - parallaxFactor);
 
-        Vector3 newPos = startPosition + travel * parallaxFactor;
-        transform.position = new Vector3(newPos.x, newPos.y, startZPos);
+        Vector2 newPos = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(xAxis ? newPos.x : startPosition.x, yAxis ? newPos.y : startPosition.y, startZPos);
 
-        if (temp > startPosition.x + length) startPosition.x += length;
-        else if (temp < startPosition.x - length) startPosition.x -= length;
+        if(infiniteLoop)
+        {
+            Vector2 totalTravel = cam.transform.position - transform.position;
+            float boundsOffset = (viewWidth / 2) * (totalTravel.x > 0 ? 1 : -1);
+            float screens = (int)((totalTravel.x + boundsOffset) / viewWidth);
+            transform.position += new Vector3(screens * viewWidth, 0);
+        }
+
     }
 }
